081d63ce9812bd40db78c22eb5d337341a4d05b1
4am-walking <chanman0808@gmail.com>
2023-12-06 21:54:40 -0500

    added logic

--- day4/problem1/solution.py
+++ day4/problem1/solution.py
@@ -1,18 +1,31 @@
 import re
 
-
 def matchesToList(pattern, line):
     matches = pattern.findall(line)
     for match in matches:
         num = match.split()
-        print(num)
     return num
 
-
-winningNumPattern = re.compile(r":\s*(\d+\s+\d+\s+\d+\s+\d+\s+\d+)\s*\|")
-scratcherPattern = re.compile(r"(\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+)")
 matches = 0
-with open("example.txt", "r") as file:
+result = 0
+winningNumPattern = re.compile(r":\s*(\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+)\s*\|")
+scratcherPattern = re.compile(r"(\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+\s+\d+)")
+with open("input.txt", "r") as file:
     for line in file:
         winningNumbers = matchesToList(winningNumPattern, line)
         scratcherNumbers = matchesToList(scratcherPattern, line)
+        counter = 0
+        temp = 1
+        for num in winningNumbers:
+            if num in scratcherNumbers:
+                counter += 1
+        if counter >= 1:
+            for _ in range(counter - 1):
+                if counter == 1:
+                    break
+                else:
+                    temp *= 2
+        else:
+            temp = 0
+        result += temp
+print(result)
